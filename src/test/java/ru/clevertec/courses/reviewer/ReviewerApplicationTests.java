28f7796b0d469e607637d039ee7d33230d54c20c
Constantine Piskunov <konstantinpiskunov1308@gmail.com>
2023-11-26 11:40:54 +0300

    init

---
+++ src/test/java/ru/clevertec/courses/reviewer/ReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.courses.reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
